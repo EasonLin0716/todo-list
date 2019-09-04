27069a5316392aed48e21a8aa27e4649ceacbe6c
Eason Lin <uu9924079@gmail.com>
2019-09-04 18:52:54 +0800

    Add Mongoose connection and create Todo model

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')                    // requiring mongoose
+
+
+
+mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true })   // setting connection to mongoDB
+
+// mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
+const db = mongoose.connection
+
+// on 替連線事件，註冊事件監聽器，只要有觸發 error 就印出 error 訊息
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// once 替連線事件，註冊「單一次」的事件監聽器，最多只會觸發一次，且觸發後會立刻解除事件監聽器。
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// 載入 todo model
+const Todo = require('./models/todo')
 
 app.get('/', (req, res) => {
   res.send('website is running')
 })
 
 app.listen(3000, () => {
-  console.log(`express is running on http://localhost:3000`)
+  console.log('Website is running at http://localhost:3000')
 })
