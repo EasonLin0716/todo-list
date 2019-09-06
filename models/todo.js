c80344d8c5e5c7106dd0303273ead6f4a8b5eaae
Eason Lin <uu9924079@gmail.com>
2019-09-06 15:28:19 +0800

    Add done in model, and create checkbox in index, detail, and edit pages

--- models/todo.js
+++ models/todo.js
@@ -5,6 +5,10 @@ const todoSchema = new Schema({
   name: {
     type: String,
     required: true
+  },
+  done: { // 完成狀態
+    type: Boolean,
+    default: false // 預設完成狀態為尚未完成
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
